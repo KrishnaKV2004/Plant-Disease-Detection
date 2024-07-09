4d6d125b00836a7af0a9ea2ef579d6e6c0550d6a
Krishna Verma <88701788+KrishnaKV2004@users.noreply.github.com>
2024-07-09 18:02:57 +0530

    Update Plant-Disease.py

--- Plant-Disease.py
+++ Plant-Disease.py
@@ -9,7 +9,7 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 def model_prediction(test_image):
-    model = tf.keras.models.load_model('trained_plant_disease_model.keras')
+    model = tf.keras.models.load_model('Plant-Disease-Model.keras')
     image = tf.keras.preprocessing.image.load_img(test_image, target_size = (128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
